8e7655bdc60925a740b4578d8ff679a9f04ab33c
sophietrefely <s.trefely@gmail.com>
2015-04-20 16:27:01 +1000

    Excluded data for n<2, calculated MAD
    Made stringent dataset including only phosphosites quantified with n>1.
    Calculated Median, max, min and number of phosphorites for ‘All Data’
    and for ’Stringent Data’.
    Calculated Median Absolute Deviation.

--- ins_reg_metab.py
+++ ins_reg_metab.py
@@ -42,7 +42,7 @@ print(clean_data_list[1:5])
 
 ##2 create z-factors for each line of data
 
-#Find mean for each line and assign to uniprot ID, ie column [0]. Add standard deviation (stdev)
+#Find mean for each line (phosphosite) and assign to uniprot ID, ie column [0]. Add standard deviation (stdev)
 import statistics
 stats_data_list = []
 for line in clean_data_list:
@@ -56,17 +56,99 @@ for line in clean_data_list:
         stdev = 0
     else:
         stdev = statistics.stdev(float_data)
-    #extend instead of append twice so [0] = uniprot id, [1] = mean, [2] = stdev.
+    #extend instead of append twice so [0] = uniprot id, [1] = mean, [2] = stdev, [3] = n.
     stats_data_line.extend([mean,stdev,n])
     stats_data_list.append(stats_data_line)
 
 print(stats_data_list[1:5])
+# Find min, max and median
+import numpy as np
+means1 = []
+for item in stats_data_list:
+    mean = item[1]
+    means1.append(mean)   
+minimum = np.amin(means1)
+print('Stats for All Data')
+print('minimum is', minimum)
+maximum = np.amax(means1)
+print('maximum is', maximum)
+median = np.median(means1)
+print('median is', median)
+sites = len(stats_data_list)
+print(sites, 'phosphosites')
 
-#plot the data to look at close to 1 vs regulated.
+#plot the data to look at close to 0 vs regulated.
 import matplotlib.pyplot as plt
-plt.hist([item[1] for item in stats_data_list if item[1]<10], bins=100, normed=True)
+plt.hist([item[1] for item in stats_data_list if item[1]<10], bins=100, color = '0.75', label='All data')
+plt.title('All Data')
+plt.xlabel('Mean log2(insulin/basal) phosphorylation')
+plt.ylabel('# of phosphorylation sites')
+
+#plot control sites as dot plot that can overlay onto the histogram.
+#Used controls used as examples for validation in Humphrey et al:
+# positive control (increase w insulin): AKT1/2 T308, TSC2 S981, TSC2 T1462, PRAS40 T246, GSK3a S21,
+# negative control (decrease w insulin):
+
+poscontrol = [1, 2.0, 3.0, 3.5, 5.0, 6.0]
+yaxisas1= [1000,1000,1000,1000,1000,1000]
+negcontrol = [-1, -2, -4, -2.5, - 5, -2.25]
+plt.plot(poscontrol, yaxisas1, marker='o', linestyle='None', color='b', label='Positive controls')
+plt.plot(negcontrol, yaxisas1, marker='o', linestyle='None', color='r', label='Negative controls')
+plt.legend()
 plt.show()
 
-#plot the data to look at close to 1 vs regulated. NB down regulated will need to be log transformed (next step)       
+#I only want high confidence proteins, therefore removed sites where n=1 from analysis
 #some proteins will have regulated and unregulated sites. Therefore, if a protein has one regulated site it will be designated regulated.
+# Another way to find 'non-insulinregulated proteins' would be to use the proteome list generated in this study and sutract reulated proteins from it.
+stringent_data_list = []
+for item in stats_data_list:
+    if item[3]>1: #ie n>1
+        stringent_data_list.append(item)
 
+print(stringent_data_list[1:5])
+
+stringent_means = []
+for item in stringent_data_list:
+    mean = item[1]
+    stringent_means.append(mean)   
+
+minimum = np.amin(stringent_means)
+print('Stats for n>1 Data')
+print('minimum is', minimum)
+maximum = np.amax(stringent_means)
+print('maximum is', maximum)
+median = np.median(stringent_means)
+print('median is', median)
+sites = len(stringent_data_list)
+print(sites, 'phosphosites')                
+    
+#plot the n>1 data 'stringent_data_list'
+plt.hist([item[1] for item in stringent_data_list], bins=100, color = '0.75', label='Stringent (n>1) data')
+plt.title('Stringent (n>1) data')
+plt.xlabel('Mean log2(insulin/basal) phosphorylation')
+plt.ylabel('# of phosphorylation sites')
+
+#plot control sites as dot plot that can overlay onto the histogram.
+#Used controls used as examples for validation in Humphrey et al:
+# positive control (increase w insulin): AKT1/2 T308, TSC2 S981, TSC2 T1462, PRAS40 T246, GSK3a S21,
+# negative control (decrease w insulin):
+
+poscontrol = [1, 2.0, 3.0, 3.5, 5.0, 6.0]
+yaxisas1= [1000,1000,1000,1000,1000,1000]
+negcontrol = [-1, -2, -4, -2.5, - 5, -2.25]
+plt.plot(poscontrol, yaxisas1, marker='o', linestyle='None', color='b', label='Positive controls')
+plt.plot(negcontrol, yaxisas1, marker='o', linestyle='None', color='r', label='Negative controls')
+plt.legend()
+plt.show()
+
+#Calculate Median Absolute Deviation (MAD) +/-2.5
+from numpy import median, absolute
+
+def mad(data, axis=None):
+    return median(absolute(data - median(data, axis)), axis)
+stringent_data_mad = mad(stringent_means)
+print('MAD:', stringent_data_mad)
+
+2.5_MAD = stringent_data_mad*2.5
+range_pos_MAD = median + 2.5_MAD
+range_neg_MAD = median - 2.5_MAD
