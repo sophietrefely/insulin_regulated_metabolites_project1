4af4a7598b0084b8cd511380ae29585cf7806673
sophietrefely <s.trefely@gmail.com>
2015-04-22 09:29:40 +1000

    Plotted all data processing
    Designated positively and negatively insulin-regulated and non
    insulin-regulated sites.

--- ins_reg_metab.py
+++ ins_reg_metab.py
@@ -7,12 +7,10 @@ exported spreadsheet as 'humphrey_20_min_data.csv'
 """
 ##1 cleaned up data
 #there are a lot of missing values denoted by 'NaN'.
-
 original_data= open("humphrey_20_min_data.csv", 'rU') 
 
 # split creates a list out of the line [<first>, <second>] field
 # [0] is gene uniprot ID, [1] first expt log2 ins/bas....[9] 9th expt. log2 ins/bas.
-
 original_data_list = []
 for line in original_data:
     stripped_line = line.rstrip()   # remove the newline from the line, it has one by default
@@ -21,7 +19,6 @@ for line in original_data:
 
 #remove lines that contain only missing data ie lines with 'NaN' for [1]...[9]
 #make new data_list
-
 data_list = []
 for line in original_data_list:
     #call all function 'all' () on list comprehension []
@@ -40,8 +37,6 @@ for line in data_list:
 
 print(clean_data_list[1:5])
 
-##2 create z-factors for each line of data
-
 #Find mean for each line (phosphosite) and assign to uniprot ID, ie column [0]. Add standard deviation (stdev)
 import statistics
 stats_data_list = []
@@ -59,10 +54,9 @@ for line in clean_data_list:
     #extend instead of append twice so [0] = uniprot id, [1] = mean, [2] = stdev, [3] = n.
     stats_data_line.extend([mean,stdev,n])
     stats_data_list.append(stats_data_line)
-
 print(stats_data_list[1:5])
 
-# Find min, max and median
+# Find min, max, median etc
 import numpy as np
 means1 = []
 for item in stats_data_list:
@@ -82,18 +76,12 @@ print('stddev is', stddev)
 sites = len(stats_data_list)
 print(sites, 'phosphosites')
 
-#plot the data to look at close to 0 vs regulated.
-import matplotlib.pyplot as plt
-plt.hist([item[1] for item in stats_data_list if item[1]<10], bins=100, color = 'b', label='All data')
-plt.title('All Data')
-plt.xlabel('Mean log2(insulin/basal) phosphorylation')
-plt.ylabel('# of phosphorylation sites')
-plt.show()
-
 #Should also consider excluding sites with a stddev above a certain cutoff.
-#plot std dev
+
+#plot std dev for All data
+import matplotlib.pyplot as plt
 plt.hist([item[2] for item in stats_data_list], bins=100, color = '0.75', label='std dev')
-plt.title('Std Dev')
+plt.title('Std Dev for All Data')
 plt.xlabel('Std Dev')
 plt.ylabel('# of phosphorylation sites')
 plt.show()
@@ -104,7 +92,6 @@ stringent_data_list = []
 for item in stats_data_list:
     if item[3]>1: #ie n>1
         stringent_data_list.append(item)
-
 print(stringent_data_list[1:5])
 
 stringent_means = []
@@ -126,47 +113,61 @@ print('stddev is', stringent_stddev)
 sites = len(stringent_data_list)
 print(sites, 'phosphosites')
 
-#convert 'Mean log2(insulin/basal) phosphorylation' into z-scores. For each mean -item[1]- (z=(item[1]-overallmean)/overallstddev))
-for item in stringent_data_list:
-    z = (item[1]-stringent_mean)/stringent_stddev
-    item[1] = z
-print(stringent_data_list[1:5])
+###convert 'Mean log2(insulin/basal) phosphorylation' into z-scores. For each mean -item[1]- (z=(item[1]-overallmean)/overallstddev))
+##for item in stringent_data_list:
+##    z = (item[1]-stringent_mean)/stringent_stddev
+##    item[1] = z
+##print(stringent_data_list[1:5])
 
-#Calculate Median Absolute Deviation (MAD) +/-2.5 from stringent_data_list z-scores
+#Calculate Median Absolute Deviation (MAD) +/-2.5 from stringent_data_list means
 from numpy import median, absolute
 
 def mad(data, axis=None):
     return median(absolute(data - median(data, axis)), axis)
-z_score_list = []
-for item in stringent_data_list:
-    z=item[1]
-    z_score_list.append(z)
-    
-z_score_mad = float(mad(z_score_list))
-print('MAD:', z_score_mad)
+##z_score_list = []
+##for item in stringent_data_list:
+##    z=item[1]
+##    z_score_list.append(z)
+##    
+##z_score_mad = float(mad(z_score_list))
+##print('MAD:', z_score_mad)
+##
+##MAD2_5 = 2.5*(z_score_mad)
+##print('2.5*MAD:', MAD2_5)
+MAD = float(mad(stringent_means))
+print('MAD:', MAD)
 
-MAD2_5 = 2.5*(z_score_mad)
+MAD2_5 = 2.5*(MAD)
 print('2.5*MAD:', MAD2_5)
 
+#plot data to compare original and stringent and show cutoff lines for MAD and positive control proteins
+import matplotlib.pyplot as plt
+plt.hist([item[1] for item in stats_data_list if item[1]<10], bins=100, color = '0.75', label='All data')
+plt.title('Data Processing')
+plt.xlabel('Mean log2(insulin/basal) phosphorylation')
+plt.ylabel('# of phosphorylation sites')
 #plot the n>1 data z-scores in 'stringent_data_list'
-plt.hist([item[1] for item in stringent_data_list], bins=100, color = '0.75', label='Stringent (n>1) data')
-plt.title('Stringent (n>1) data')
+plt.hist([item[1] for item in stringent_data_list], bins=100, color = 'b', label='Stringent (n>1) data')
 plt.xlabel('Mean log2(insulin/basal) phosphorylation')
 plt.ylabel('# of phosphorylation sites')
 #overlay MAD*2.5 barrier lines
-plt.plot([MAD2_5, MAD2_5],[0, 1000], 'r--')
-plt.plot([-MAD2_5, -MAD2_5],[0, 1000], 'r--')
-
-#plot control sites as dot plot that can overlay onto the histogram.
+plt.plot([MAD2_5, MAD2_5],[0, 5000], 'r--')
+plt.plot([-MAD2_5, -MAD2_5],[0, 5000], 'r--', label='Stringent data +/-2.5*MAD')
+#overlay MAD*1 barrier lines
+plt.plot([MAD, MAD],[0, 5000], 'm--')
+plt.plot([-MAD, -MAD],[0, 5000], 'm--', label='Stringent data +/-MAD')
+#overlay control phosphosites:
 #Used controls used as examples for validation in Humphrey et al:
 # positive control (known Akt pathway sites increase w insulin): AKT1/2 T308, TSC2 S981, TSC2 T1462, PRAS40 T246, GSK3a S21, GSK3b S9, ENOS S1177, PFKFB2 S466, AS160 T588, AS160 S642, AS160 S318, AS250 T715
-# negative control (decrease w insulin):
-
 poscontrol = [1, 2.0, 3.0, 3.5, 5.0, 6.0]
-yaxisas1= [1000,1000,1000,1000,1000,1000]
-negcontrol = [-1, -2, -4, -2.5, - 5, -2.25]
-plt.plot(poscontrol, yaxisas1, marker='o', linestyle='None', color='b', label='Positive controls')
-plt.plot(negcontrol, yaxisas1, marker='o', linestyle='None', color='r', label='Negative controls')
+yaxisas1= [100,100,100,100,100,100]
+plt.plot(poscontrol, yaxisas1, marker='o', linestyle='None', color='r', label='Positive controls')
+#annotate divisions for insulin-regulated/non-regulated lists
+plt.annotate('non-regulated sites', xy=(0, 2500), xytext=(-5, 2600),
+            arrowprops=dict(facecolor='k', shrink=0.05),
+            )
+plt.text(-7, 2000, 'negatively regulated sites')
+plt.text(3, 2000, 'positively regulated sites')
 plt.legend()
 plt.show()
 
